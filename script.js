b6cdf698a26b9ebaf8bee68ade0259fcdcc79523
DileepKumarRambarki <rambarkidileepkumar@gmail.com>
2025-07-15 19:20:51 +0530

    sorted ranks

--- script.js
+++ script.js
@@ -1,33 +1,10 @@
-// fetch('last_rank_2025.json')
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log("College data loaded:", data[0]);
 
-//     // Example usage:
-//     const userRank = 5000;
-//     const userBranch = 'CSE';
-//     const userCategory = 'BCB_BOYS';
-
-//     const margin = 5000;
-//     const matches = data.filter(college =>
-//       college.branch === userBranch &&
-//       Math.abs(college[userCategory] - userRank) <= margin
-//     );
-//     matches.map((clg)=>{
-//         const clg_name=clg['NAME OF THE INSTITUTION'];
-//         const clg_code=clg['INSTCODE'];
-//         const place=clg["PLACE"];
-//         const lastRank=clg[userCategory]
-//         console.log("->",clg_code,clg_name,place,userBranch,userCategory,lastRank);
-//     })
-//   })
-//   .catch(err => console.error("Error loading JSON:", err));
 function predictColleges() {
     const userRank = parseInt(document.getElementById('rank').value);
     const userBranch = document.getElementById('branch').value;
     const userCategory = document.getElementById('category').value.toUpperCase();
   
-    const margin = 5000;
+    const margin = 1000;
   
     fetch('last_rank_2025.json')
       .then(response => response.json())
@@ -35,9 +12,9 @@ function predictColleges() {
         const matches = data.filter(college =>
           college.branch === userBranch &&
           !isNaN(college[userCategory]) &&
-          Math.abs(college[userCategory] - userRank) <= margin
+          college[userCategory]-userRank>=-margin
         );
-  
+          matches.sort((a,b)=>a[userCategory]-b[userCategory]);
         const table = document.getElementById('resultTable');
         const tbody = table.querySelector('tbody');
         tbody.innerHTML = '';
